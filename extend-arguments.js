f20399516f82f543e7db90d205c95f96dd198c73
Ginden <michalwadas@gmail.com>
2014-07-19 10:48:10 +0200

    Creating...

---
+++ extend-arguments.js
@@ -0,0 +1,67 @@
+// if the module has no dependencies, the above pattern can be simplified to
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.extendArguments = factory();
+  }
+}(this, function () {
+    'use strict';
+    
+    var MAGIC_ARGUMENTS_TO_STRING = (function(){
+        return Object.prototype.toString.call(arguments);
+    })();
+    
+    function isArgumentsObject(what) {
+        return Object.prototype.toString.call(what) === MAGIC_ARGUMENTS_TO_STRING;        
+    }
+    
+    var storage = Object.create(null);
+    var argumentsProto = Object.create(null);
+    
+    window.debug = {
+        storage: storage,
+        argumentsProto: argumentsProto
+    };
+    
+    function mockingGetter(propName) {
+        if (isArgumentsObject(this)) {
+            return argumentsProto[propName];
+        }
+        else {
+            return storage[propName];
+        }
+    }
+    
+    function mockingSetter(propName, value) {
+        storage[propName] = value[0];
+        return value;
+    }
+    
+    function simpleCurry(func) {
+        var args = Array.prototype.slice.call(arguments,1);
+        return function() {
+            return func.apply(this, args.concat(arguments));
+        }
+    }
+    
+    return function extendArguments(name, value) {
+        if (Object.prototype[name]) {
+            storage[name] = Object.prototype[name]
+        }
+        Object.defineProperty(Object.prototype, name, {
+            set: simpleCurry(mockingSetter, name),
+            get: simpleCurry(mockingGetter, name),
+            enumerable: false,
+            configurable: true
+        });
+        argumentsProto[name] = value;
+    };
+}));
